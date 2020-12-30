62cb56408b32753b27edfe9f7e8a2da306763ea1
Luky Luke <acab@at.com>
2020-12-30 21:14:12 +0100

    Clean up and implemented file_endings_to_be_deleted

--- sortier_ma_was.py
+++ sortier_ma_was.py
@@ -1,3 +1,8 @@
+#
+#
+#Care: recheck file_endings_to_be_deleted
+
+
 import os
 from os import listdir
 
@@ -5,16 +10,17 @@ root_movie_folder='E:\\Filme'
 
 movie_list= sorted(listdir(root_movie_folder))
 
-# # for movie_list in root_movie_folder:
-# #     enumerate
-# # WIE INDEX UND ENUMERATE?
-file_endings = []
+simulation = True
+#TODO 
+# simulation fuer wenn file endung dann (move/delete -> kommentieren) 
+
+file_endings_to_be_deleted = [ '.jpg', '.nfo', '.txt', '.gz', '.png', '.YIFY', '.rar', '.idx', '.lnk', '.tbn']
 
 for movie_index, movie_titel in enumerate(movie_list):
     movie_path = os.path.join(root_movie_folder, movie_titel)
     movie_sub_folder = listdir(movie_path)
 
-    print("\n", movie_index, movie_titel, "[","Number of Files:", len(movie_sub_folder), "]") #,"Size:",folder_size.st_size,
+    # print("\n", movie_index, movie_titel, "[","Number of Files:", len(movie_sub_folder), "]") #,"Size:",folder_size.st_size,
 
     
 
@@ -30,35 +36,10 @@ for movie_index, movie_titel in enumerate(movie_list):
                 print("\t File ending non existant", file_name)
             else: 
                 file_ending = file_name[file_ending_index:]
-                # Help to find file endings, not needed for normal mode
-                if not file_ending in file_endings:
-                    file_endings.append(file_ending)
-                # print(file_name[file_ending_index:])
-
             # print("\t -", index_sub +1, file_name, " ")
 
 size=os.path.getsize(root_movie_folder)
 print(size)
 
-print(file_endings)
+print(file_endings_to_be_deleted)
 
-# IDEE FÜR COUNT
-# def count(dir, counter=0):
-#     "returns number of files in dir and subdirs"
-#     for pack in os.walk(dir):
-#         for f in pack[2]:
-#             counter += 1
-#     return dir + " : " + str(counter) + "files"
-
-# file_count= len(movie_list)
-# print(file_count)
-
- # file_count= 0
-    # for files in os.walk(movie_path):
-    #     for files in movie_path:
-    #         file_count=file_count+1
-
-# Number_Of_Files=0
-# for files in os.walk(root_movie_folder):
-#     for files in root_movie_folder:
-#         Number_Of_Files=Number_Of_Files+1
